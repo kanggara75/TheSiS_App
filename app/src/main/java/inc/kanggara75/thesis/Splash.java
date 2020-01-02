dc8a13f31162c806b7a03beb14822ab14a285ee0
KAnggara75 <KAnggara75@gmail.com>
2020-01-03 04:25:39 +0700

    Finish add Animation on Splash Screen

--- app/src/main/java/inc/kanggara75/thesis/Splash.java
+++ app/src/main/java/inc/kanggara75/thesis/Splash.java
@@ -2,16 +2,33 @@ package inc.kanggara75.thesis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
+
+    private ImageView iv1, iv2, iv3;
+
+    Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        iv1 = (ImageView) findViewById(R.id.iv1);
+        iv2 = (ImageView) findViewById(R.id.iv2);
+        iv3 = (ImageView) findViewById(R.id.iv3);
+
+        animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.push_down);
+        iv1.setAnimation(animation);
+
+        animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.pus_right);
+        iv2.setAnimation(animation);
+        iv3.setAnimation(animation);
 
         Thread thread = new Thread()
         {
@@ -20,7 +37,7 @@ public class Splash extends AppCompatActivity {
             {
                 try
                 {
-                    sleep(4000);
+                    sleep(1850);
                     Intent intent = new Intent(getApplicationContext(), Login.class);
                     startActivity(intent);
                     finish();
