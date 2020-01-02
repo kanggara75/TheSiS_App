8d4fdfaefefd394835906733f12297bbfb6b1a20
KAnggara75 <KAnggara75@gmail.com>
2020-01-03 03:41:55 +0700

    Delete MainActivity and make Splas as default activity

--- app/src/main/java/inc/kanggara75/thesis/Splash.java
+++ app/src/main/java/inc/kanggara75/thesis/Splash.java
@@ -2,6 +2,8 @@ package inc.kanggara75.thesis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
@@ -10,5 +12,26 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    sleep(4000);
+                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                    startActivity(intent);
+                    finish();
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+
+                super.run();
+            }
+        };
+        thread.start();
     }
 }
